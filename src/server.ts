9237308e23e368b85890985d2c6b9508aaa50642
Livia Giro <lgmayrinck@gmail.com>
2024-05-30 17:05:26 -0300

    add ordem de gênero, titulo e linguagem

--- src/server.ts
+++ src/server.ts
@@ -4,10 +4,13 @@ import { PrismaClient } from "@prisma/client";
 const port = 3000;
 const app = express();
 const prisma = new PrismaClient();
-app.get("/movies", async (req, res) => {
-    const movies = await prisma.movie.findMany();
+app.get('/movies', async (_, res) => {
+    const movies = await prisma.movie.findMany({
+      orderBy: { title: 'asc' },
+      include: { genres: true, languages: true },
+    });
     res.json(movies);
-});
+   });
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
