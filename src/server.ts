cd2a6ac39fa19d1e6ac28d1792014594e0f1bcfa
Livia Giro <lgmayrinck@gmail.com>
2024-05-30 18:04:07 -0300

    mensagem de erro para evitar duplicados

--- src/server.ts
+++ src/server.ts
@@ -4,12 +4,34 @@ import { PrismaClient } from "@prisma/client";
 const port = 3000;
 const app = express();
 const prisma = new PrismaClient();
+
+app.use(express.json());
 app.get('/movies', async (_, res) => {
     const movies = await prisma.movie.findMany({
       orderBy: { title: 'asc' },
       include: { genres: true, languages: true },
     });
     res.json(movies);
+   });
+
+   app.post('/movies', async (req, res) => {
+
+    const { title, genre_id, language_id, oscar_count, release_date } = req.body;
+   
+    try {
+      await prisma.movie.create({
+        data: {
+          title: title,
+          genre_id: genre_id,
+          language_id: language_id,
+          oscar_count: oscar_count,
+          release_date: new Date(release_date),
+        },
+      });
+    } catch (error) {
+      return res.status(500).send({message: "Falha ao cadastrar o filme"});
+    }  
+    res.status(201).send();
    });
 
 app.listen(port, () => {
